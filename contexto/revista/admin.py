55cd5d5451736473eb754070ba1fa1c2ad5b4d5a
Leonardo Vidarte <lvidarte@gmail.com>
2011-08-15 02:22:05 -0300

    Primer commit

---
+++ contexto/revista/admin.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.contrib import admin
+from django.forms import TextInput, Textarea, ModelForm, Media
+
+from contexto.revista.models import *
+
+RESULTS_PER_PAGE = 50
+TEXTAREA_SIZE = 60
+
+# {{{ ArchivoAdmin
+class ArchivoAdmin(admin.ModelAdmin):
+    list_per_page = RESULTS_PER_PAGE
+    filter_horizontal = ('tags',)
+    list_display = (
+        'file', 'thumbnail', 'get_size', 'width', 'height',
+        'editor_creacion', 'get_fecha_creacion',
+        'get_editor_modificacion', 'get_fecha_modificacion',
+    )
+    list_filter = ('fecha_creacion', 'editor_creacion', 'is_image')
+    fieldsets = (
+        (None, {
+            'fields': ('file', 'alt'),
+        }),
+        ('Descripción', {
+            'classes': ('collapse',),
+            'fields': ('descripcion',),
+        }),
+        ('Tags', {
+            'classes': ('collapse',),
+            'fields': ('tags',),
+        }),
+    )
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':TEXTAREA_SIZE})},
+    }
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.editor_creacion = request.user
+        obj.editor_modificacion = request.user
+        obj.save()
+# }}}
+# {{{ AutorAdmin
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'slug', 'email', 'web', 'get_estado')
+    prepopulated_fields = {'slug': ('nombre', 'apellido')}
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'apellido', 'slug', 'email', 'web'),
+        }),
+        (None, {
+            'fields': ('foto',),
+        }),
+        (None, {
+            'fields': ('tipo', 'estado',),
+        }),
+        ('Extra', {
+            'classes': ('collapse', ),
+            'fields': ('comentario',),
+        }),
+    )
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':TEXTAREA_SIZE})},
+    }
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+# }}}
+# {{{ NotaAdmin
+class NotaAutoresInline(admin.TabularInline):
+    model = NotaAutores
+    extra = 0
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'autor':
+            kwargs['queryset'] = Autor.objects.order_by('apellido', 'nombre')
+        return db_field.formfield(**kwargs)
+
+class NotaArchivosInline(admin.TabularInline):
+    model = NotaArchivos
+    extra = 0
+
+class NotaAdmin(admin.ModelAdmin):
+    inlines = (NotaAutoresInline, NotaArchivosInline)
+    date_hierarchy = 'fecha'
+    list_display = (
+        'id', 'fecha', 'hora', 'titulo',
+        'es_galeria', 'orden', 'get_estado'
+    )
+    list_filter = ('fecha', 'editor_creacion')
+    search_fields = ['@titulo',]
+    filter_horizontal = ('tags', 'relacionadas')
+    prepopulated_fields = {'slug': ('titulo',)}
+    fieldsets = (
+        ('Fecha y Hora', {
+            'classes': ('collapse', ),
+            'fields': ('fecha', 'hora'),
+        }),
+        (None, {
+            'fields': ('titulo', 'slug', 'copete', 'cuerpo'),
+        }),
+        (None, {
+            'fields': ('estado', 'jerarquia', 'es_galeria', 'orden'),
+        }),
+        ('Tags', {
+            'classes': ('collapse', ),
+            'fields': ('tags',),
+        }),
+        ('Notas relacionadas', {
+            'classes': ('collapse', ),
+            'fields': ('relacionadas',),
+        }),
+    )
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':TEXTAREA_SIZE})},
+        models.TextField: {'widget': Textarea(attrs={'rows':'20', 'cols':'80'})}
+    }
+
+    def save_model(self, request, obj, form, change):
+        #import ipdb; ipdb.set_trace()
+        if not change:
+            obj.editor_creacion = request.user
+        obj.editor_modificacion = request.user
+
+        obj.save()
+# }}}
+# {{{ TagAdmin
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'slug', 'get_en_menu', 'padre', 'orden')
+    list_filter = ('en_menu',)
+    prepopulated_fields = {'slug': ('nombre',)}
+# }}}
+
+admin.site.register(Archivo, ArchivoAdmin)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Nota, NotaAdmin)
+admin.site.register(Tag, TagAdmin)
+
---
+++ contexto/revista/models.py
@@ -0,0 +1,306 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import User as Editor
+
+from PIL import Image
+from mimetypes import guess_type
+from datetime import datetime
+
+SINO_CHOICES = (
+    (True, 'Si'),
+    (False, 'No'),
+)
+
+TIPOS_CHOICES = (
+    ('fotografo', 'Fotógrafo'),
+    ('periodista', 'Periodista'),
+    ('agencia', 'Agencia'),
+    ('otro', 'Otro'),
+)
+
+JERARQUIA_CHOICES = (
+    ('normal', 'Normal'),
+    ('destacada', 'Destacada'),
+    ('secundaria', 'Secundaria'),
+)
+
+
+class Archivo(models.Model): # {{{
+    file = models.FileField(upload_to='archivos/%Y/%m/%d', max_length=512,
+        verbose_name='archivo')
+    alt = models.CharField(max_length=256, blank=True,
+        verbose_name='alt')
+    descripcion = models.TextField(blank=True,
+        verbose_name='descripción')
+    size = models.IntegerField(blank=True, default=0,
+        verbose_name='tamaño')
+    mime = models.CharField(max_length=256, blank=True)
+    width = models.IntegerField(blank=True, default=0,
+        verbose_name='ancho')
+    height = models.IntegerField(blank=True, default=0,
+        verbose_name='alto')
+    is_image = models.BooleanField(blank=True, default=False,
+        verbose_name='imagen')
+    autor = models.ForeignKey('Autor', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
+
+    # Datos creación
+    editor_creacion = models.ForeignKey(Editor, blank=True,
+        related_name='archivo_creado_por', verbose_name='creado por')
+    fecha_creacion = models.DateTimeField(auto_now_add=True,
+        verbose_name='creado el')
+
+    # Datos última modificación
+    editor_modificacion = models.ForeignKey(Editor, blank=True,
+        related_name='archivo_modificado_por', verbose_name='modificado por')
+    fecha_modificacion = models.DateTimeField(auto_now=True,
+        verbose_name='modificado el')
+
+    class Meta:
+        verbose_name_plural = 'archivos'
+
+    def es_imagen(self):
+        return self.is_image
+
+    def get_fecha_modificacion(self):
+        if self.fecha_modificacion == self.fecha_creacion:
+            return ''
+        else:
+            return self.fecha_modificacion.strftime(settings.TXT_DATETIME_FORMAT)
+
+    get_fecha_modificacion.short_description = 'fecha modificación'
+
+    def get_editor_modificacion(self):
+        if self.fecha_modificacion == self.fecha_creacion:
+            return ''
+        else:
+            return self.editor_modificacion
+
+    get_editor_modificacion.short_description = 'modificado por'
+
+    def get_fecha_creacion(self):
+        return self.fecha_creacion.strftime(settings.TXT_DATETIME_FORMAT)
+
+    get_fecha_creacion.short_description = 'fecha creación'
+    get_fecha_creacion.admin_order_field = 'fecha_creacion'
+
+    def get_size(self):
+        if self.size < 1024: # < 1Kib
+            return "%s bytes" % self.size
+        elif self.size < 1048576: # < 1Mib
+            return "%s Kib" % round(self.size / float(1024), 1)
+        else: # >= 1Mib
+            return "%s Mib" % round(self.size / float(1048576), 1)
+
+    get_size.short_description = 'tamaño'
+    get_size.allow_tags = True
+    get_size.admin_order_field = 'size'
+
+    def thumbnail(self):
+        if self.is_image:
+            return u'<img src="/static%s" width="100px" />' % self.file.url
+
+    thumbnail.short_description = 'miniatura'
+    thumbnail.allow_tags = True
+
+    def get_absolute_url(self):
+        return self.file.url
+
+    def save(self, force_insert=False, force_update=False):
+        setattr(self, 'size', self.file.size) 
+        mime = guess_type(self.file.name)[0]
+        setattr(self, 'mime', mime) 
+
+        if mime.split('/')[0] == 'image':
+            setattr(self, 'is_image', 1) 
+            img = Image.open(self.file)
+            width, height = img.size
+            setattr(self, 'width', width) 
+            setattr(self, 'height', height) 
+
+        super(Archivo, self).save(force_insert, force_update)
+
+    def __unicode__(self):
+        return self.file.name
+# }}}
+class Autor(models.Model): # {{{
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256, blank=True)
+    slug = models.SlugField(max_length=256, unique=True,
+        help_text='URL del listado de notas del autor')
+    email = models.EmailField(max_length=128, blank=True)
+    web = models.URLField(max_length=256, blank=True,
+        help_text='Blog/página web personal')
+    comentario = models.TextField(max_length=512, blank=True)
+    foto = models.OneToOneField('Archivo', blank=True, null=True, related_name='+')
+    tipo = models.CharField(max_length=64, default='periodista',
+        choices=TIPOS_CHOICES)
+    estado = models.BooleanField(default=True,
+        choices=SINO_CHOICES, verbose_name='activo')
+
+    class Meta:
+        verbose_name_plural = 'autores'
+
+    def get_estado(self):
+        return self.estado
+
+    get_estado.short_description = 'activo'
+    get_estado.boolean = True
+    get_estado.admin_order_field = 'estado'
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('pagina12-diario-autores-autor', (), {'directorio': self.directorio})
+
+    def __unicode__(self):
+        return " ".join([x for x in (self.nombre, self.apellido) if x])
+# }}}
+class Nota(models.Model): # {{{
+    fecha = models.DateField(default=datetime.now())
+    hora = models.TimeField(default=datetime.now())
+    autores = models.ManyToManyField('Autor', blank=True, null=True,
+        through='NotaAutores')
+    titulo = models.CharField(max_length=256, blank=True,
+        verbose_name='Título')
+    slug = models.SlugField(max_length=256, unique_for_date='fecha',
+        help_text='URL de la nota (debería ser el título de la nota)')
+    copete = models.TextField(blank=True)
+    cuerpo = models.TextField(blank=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    archivos = models.ManyToManyField('Archivo', blank=True, null=True,
+        through='NotaArchivos')
+    es_galeria = models.BooleanField(default=False, choices=SINO_CHOICES,
+        verbose_name='Es galería')
+    orden = models.IntegerField(default=0)
+    estado = models.BooleanField(default=False,
+        choices=SINO_CHOICES, verbose_name='visible')
+    jerarquia = models.CharField(max_length=64, default='normal',
+        choices=JERARQUIA_CHOICES, verbose_name='jerarquía')
+    relacionadas = models.ManyToManyField('Nota', blank=True, null=True,
+        verbose_name='notas relacionadas')
+
+    # Datos creación
+    editor_creacion = models.ForeignKey(Editor, blank=True,
+        related_name='nota_creado_por', verbose_name='creado por')
+    fecha_creacion = models.DateTimeField(auto_now_add=True,
+        verbose_name='creado el')
+
+    # Datos última modificación
+    editor_modificacion = models.ForeignKey(Editor, blank=True,
+        related_name='nota_modificado_por', verbose_name='modificado por')
+    fecha_modificacion = models.DateTimeField(auto_now=True,
+        verbose_name='modificado el')
+
+    # Manager
+    #objects = PublicManagerNota()
+
+    def get_estado(self):
+        return self.estado
+
+    get_estado.short_description = 'visible'
+    get_estado.boolean = True
+    get_estado.admin_order_field = 'estado'
+
+    @models.permalink
+    def get_absolute_url(self):
+        #import pdb; pdb.set_trace()
+        slug = self.slug if self.slug else 'sin-titulo'
+        return ('pagina12-diario-nota', (), {
+                    'publicacion': self.publicacion.directorio,
+                    'seccion': self.seccion.directorio,
+                    'fecha': Fecha(self.fecha).amd(),
+                    'nota_id': self.id,
+                    'slug': slug})
+
+    def get_archivos(self):
+        return self.notaarchivos_set.all()
+
+    def get_anterior(self):
+        notas = Nota.objects.filter(
+                        publicacion=self.publicacion,
+                        seccion=self.seccion,
+                        fecha__lt=self.fecha).order_by('-fecha')[:1]
+        if len(notas):
+            return notas[0]
+
+    def get_siguiente(self):
+        notas = Nota.objects.filter(
+                        publicacion=self.publicacion,
+                        seccion=self.seccion,
+                        fecha__gt=self.fecha).order_by('fecha')[:1]
+        if len(notas):
+            return notas[0]
+
+    def save(self, force_insert=False, force_update=False):
+        if self.slug == '':
+            self.slug = 'sin-titulo'
+        super(Nota, self).save(force_insert, force_update)
+
+    def __unicode__(self):
+        return '%s [%s] %s' % (self.id,
+            self.fecha.strftime('%Y-%m-%d'), self.titulo)
+# }}}
+class NotaArchivos(models.Model): # {{{
+    nota = models.ForeignKey('Nota')
+    archivo = models.ForeignKey('Archivo')
+    epigrafe = models.TextField(blank=True,
+        verbose_name='epígrafe')
+    nombre = models.CharField(max_length=128, blank=True)
+    orden = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'revista_nota_archivos'
+        verbose_name = 'archivo de la nota'
+        verbose_name_plural = 'archivos de la nota'
+
+    def thumbnail(self, width=100):
+        return self.archivo.thumbnail(width)
+
+    thumbnail.short_description = 'miniatura'
+    thumbnail.allow_tags = True
+
+    def __unicode__(self):
+        return self.archivo.__unicode__()
+
+    def get_autores(self):
+        return self.archivo.ArchivoAutores_set.all()
+
+# }}}
+class NotaAutores(models.Model): # {{{
+    nota = models.ForeignKey('Nota')
+    autor = models.ForeignKey('Autor')
+    orden = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'revista_nota_autores'
+        verbose_name = 'autor de la nota'
+        verbose_name_plural = 'autores de la nota'
+
+    def __unicode__(self):
+        return ", ".join((self.autor.apellido, self.autor.nombre))
+# }}}
+class Tag(models.Model): # {{{
+    nombre = models.CharField(max_length=128,
+        verbose_name='nombre')
+    slug = models.SlugField(max_length=256, unique=True,
+        help_text='URL del listado de notas relacionadas con el tag')
+    en_menu = models.BooleanField(default=False,
+        choices=SINO_CHOICES, verbose_name='En menú?',
+        help_text='¿Se muestra en el menú?')
+    padre = models.OneToOneField('Tag', blank=True, null=True,
+        help_text='¿De quién es submenú?')
+    orden = models.IntegerField(default=0)
+
+    def get_en_menu(self):
+        return self.en_menu
+
+    get_en_menu.short_description = 'En menú'
+    get_en_menu.boolean = True
+    get_en_menu.admin_order_field = 'en_menu'
+
+    def __unicode__(self):
+        return self.nombre
+# }}}
+
