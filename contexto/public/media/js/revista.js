b71e7857b2fe53eadf69e41741818a437fcf8e17
Leonardo Vidarte <lvidarte@gmail.com>
2011-08-30 09:19:19 -0300

    Arreglo en generación del menú

--- contexto/public/media/js/revista.js
+++ contexto/public/media/js/revista.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+
+	/**
+	 * Guías
+	 */
 	$('#grid').click(function(event) {
 		$('#container').toggleClass('grid');
 		event.preventDefault();
 	});
 
+	/**
+	 * Galería
+	 */
 	$("a[rel^='prettyPhoto']").prettyPhoto();
+
+	/**
+	 * Menú
+	 */
+	$('#menu li > a').click(function(event) {
+		event.preventDefault();
+		$('.submenu').hide();
+		$('#' + this.getAttribute('rel')).show();
+	});
+
+	$('.submenu').hide();
+	$('#' + submenu).show();
+
 });
