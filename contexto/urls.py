757b14de3e22947e8aa48ff44ec3caf2da37fdbc
Leonardo Vidarte <lvidarte@gmail.com>
2012-02-11 14:10:49 -0300

    Seteos para servir sitio en desarrollo.

--- contexto/urls.py
+++ contexto/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,8 +8,17 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'contexto.views.home', name='home'),
 
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cache/', include('contexto.cache.urls')),
     url(r'', include('contexto.revista.urls')),
 )
+
+# Serve images from media in development
+if settings.DEBUG:
+    import os.path
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(settings.PROJECT_PATH, 'media')}),
+    )
