c59f7583b5a2639f92f46cd06ac486614908f5c9
Leonardo Vidarte <lvidarte@gmail.com>
2013-10-02 20:15:43 -0300

    Update INSTALL

--- contexto/revista/views.py
+++ contexto/revista/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.views.generic import list_detail
 
